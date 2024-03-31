e5876b5766d98b4e25eb9d599206f2a39f775823
Tollan Sitotaw <tollanberhanu@gmail.com>
2024-03-31 16:22:57 +0300

    feat: add custom view - add url for view - add get method for view

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import ItemList, ItemDetail, LocationList, LocationDetail
+from .views import ItemList, ItemDetail, LocationList, LocationDetail, DescriptionList, DescriptionDetail
 
 urlpatterns = [
     path('item/', ItemList.as_view()),
     path('item/<int:pk>/', ItemDetail.as_view()),
     path('location/', LocationList.as_view()),
     path('location/<int:pk>/', LocationDetail.as_view()),
+    path('description/', DescriptionList.as_view(), name='Description List'),
+    path('description/<int:pk>/', DescriptionDetail.as_view(), name='Description Detail')
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,6 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import Item, Location
 from .serializers import ItemSerializer, LocationSerializer
 
@@ -11,7 +13,6 @@ class ItemList(generics.ListCreateAPIView):
         if location:
             # queryset = queryset.filter(item_location=location)
             queryset = Item.objects.filter(item_location=location)
-
         return queryset
 
 class ItemDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -27,3 +28,39 @@ class LocationList(generics.ListCreateAPIView):
 class LocationDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = LocationSerializer
     queryset = Location.objects.all()
+
+class DescriptionList(APIView):
+    def get(self, request, format=None):
+
+        item_filter = request.query_params.get('item', '')
+        location_filter = request.query_params.get('location', '')
+
+        items = Item.objects.filter(item_name__contains=item_filter)\
+              if item_filter else Item.objects.all()
+
+        serialized_items = ItemSerializer(items, many=True)
+        if not serialized_items.data:
+            return Response(data='Nothing to display :(', status=401)
+        
+        description = '**Summary:**\n'
+        for idx, item in enumerate(serialized_items.data):
+            item_name = item['item_name']
+
+            if location_filter:
+                if Location.objects.filter(id=item['item_location'], location_name__contains=location_filter).exists():
+                    location = Location.objects.get(id=item['item_location'], location_name__contains=location_filter)
+                else:
+                    description += f'{idx+1}. {item_name} doesn\'t exist in {location_filter}!'
+            else:
+                location = Location.objects.get(pk=item['item_location'])
+
+            if 'location' in locals():  # Check if the 'location' variable exists (probably not the best way to do this
+                serialized_location = LocationSerializer(location)
+                location_name = serialized_location.data['location_name']
+                description += f'{idx+1}. {item_name} is located in {location_name}.\n'
+
+        return Response(data=description, status=status.HTTP_200_OK)
+
+class DescriptionDetail(APIView):
+    def get():
+        pass
