cceefe11f13a8a76a9ea3d4cc97466bbfd475ff7
Tollan Sitotaw <tollanberhanu@gmail.com>
2024-03-31 17:18:17 +0300

    feat: add custom view - add item description detail view - add location description detail view - add url for views

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ItemList, ItemDetail, LocationList, LocationDetail, DescriptionList, DescriptionDetail
+from .views import ItemList, ItemDetail, LocationList, LocationDetail, DescriptionList, DescriptionItemDetail, DescriptionLocationDetail
 
 urlpatterns = [
     path('item/', ItemList.as_view()),
@@ -7,5 +7,6 @@ urlpatterns = [
     path('location/', LocationList.as_view()),
     path('location/<int:pk>/', LocationDetail.as_view()),
     path('description/', DescriptionList.as_view(), name='Description List'),
-    path('description/<int:pk>/', DescriptionDetail.as_view(), name='Description Detail')
+    path('description/item/<int:pk>/', DescriptionItemDetail.as_view(), name='Item Description Detail'),
+    path('description/location/<int:pk>/', DescriptionLocationDetail.as_view(), name='Location Description Detail')
 ]
--- api/views.py
+++ api/views.py
@@ -40,12 +40,13 @@ class DescriptionList(APIView):
 
         serialized_items = ItemSerializer(items, many=True)
         if not serialized_items.data:
-            return Response(data='Nothing to display :(', status=401)
+            return Response(data='Nothing to display :(', status=400)
         
         description = '**Summary:**\n'
         for idx, item in enumerate(serialized_items.data):
             item_name = item['item_name']
 
+            location = None
             if location_filter:
                 if Location.objects.filter(id=item['item_location'], location_name__contains=location_filter).exists():
                     location = Location.objects.get(id=item['item_location'], location_name__contains=location_filter)
@@ -54,13 +55,42 @@ class DescriptionList(APIView):
             else:
                 location = Location.objects.get(pk=item['item_location'])
 
-            if 'location' in locals():  # Check if the 'location' variable exists (probably not the best way to do this
+            if location is not None:
                 serialized_location = LocationSerializer(location)
                 location_name = serialized_location.data['location_name']
                 description += f'{idx+1}. {item_name} is located in {location_name}.\n'
 
         return Response(data=description, status=status.HTTP_200_OK)
 
-class DescriptionDetail(APIView):
-    def get():
-        pass
+class DescriptionItemDetail(APIView):
+    def get(self, request, pk):
+        if not Item.objects.filter(pk=pk).exists():
+            return Response('The item does not exist!', status=status.HTTP_404_NOT_FOUND)
+        
+        item = Item.objects.get(pk=pk)
+        serialized_item = ItemSerializer(item)
+
+        location = Location.objects.get(pk=serialized_item.data['item_location'])
+        serialized_location = LocationSerializer(location)
+
+        return Response(( f'The selected item \'{serialized_item.data['item_name']}\' '
+                          f'is located in the \'{serialized_location.data['location_name']}\'.' ),
+                         status=status.HTTP_200_OK)
+
+class DescriptionLocationDetail(APIView):
+    def get(self, request, pk):
+        if not Location.objects.filter(pk=pk).exists():
+            return Response('No such location!', status=status.HTTP_404_NOT_FOUND)
+        
+        # location = Location.objects.get(pk=pk).pk
+        location = Location.objects.get(pk=pk)
+        location = LocationSerializer(location).data
+
+        items = Item.objects.filter(item_location=location['id'])
+        serialized_items = ItemSerializer(items, many=True)
+        
+        if serialized_items.data:
+            items_in_location = ', '.join([item['item_name'] for item in serialized_items.data])
+            return Response(f'The \'{location['location_name']}\' contains: {items_in_location}.')
+        else:
+            return Response(f'This {location['location_name']} does not contain any items!', status=status.HTTP_200_OK)
